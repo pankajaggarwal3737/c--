5dfb14224fa7c91905594eb27b577432cac30705
pankajaggarwal3737 <pankaj3737.be20@chitkara.edu.in>
2022-07-28 13:21:14 +0530

    Find sum of all the possible subarrays of an Array

---
+++ FindingSumOfAllTheSubArrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        }
+    for(int i=0;i<n;i++){
+        int current =0;
+        for(int j=i;j<n;j++){
+        current = current+arr[j];
+        cout<<current<<endl;}
+    
+   }
+
+}
