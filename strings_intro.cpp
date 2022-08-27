9e3f70e14dc3aec00a57f626bbdb3b18ea6c6634
pankajaggarwal3737 <pankaj3737.be20@chitkara.edu.in>
2022-08-27 23:53:38 +0530

    String introduction

---
+++ strings_intro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s1 ="abc";
+    string s2 ="abc";
+    if(s1.compare(s2)==0)
+    cout<<"strings are equal"<<endl;
+    cout<<s2.compare(s1)<<endl;
+    return 0;
+
+}
