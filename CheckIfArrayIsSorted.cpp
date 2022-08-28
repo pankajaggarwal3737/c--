311716a3d7508dc62301443a0ff863a16396053a
pankajaggarwal3737 <pankaj3737.be20@chitkara.edu.in>
2022-08-28 19:55:59 +0530

    check if array is sorted or not

--- CheckIfArrayIsSorted.cpp
+++ CheckIfArrayIsSorted.cpp
@@ -6,19 +6,40 @@ int main(){
     int arr[N];
 
     for(int i =0; i<N; i++) cin >> arr[i];
-    bool checker = true;
-    for(int i=0; i<N-1; i++){
-        if(arr[i]<arr[i+1]){
-            checker = true;
-            // cout << "sorted" << " ";
-        }
-        else{
-            checker = false; 
-            // cout << "not sorted" <<  " ";
-            break;
+    // bool checker = true;
+
+    bool check_1 = true; 
+    bool check_2 = true;
+    int first = 0;
+    int second = 1;
+    while(arr[first]==arr[second]){
+        first++; second++;
+    }
+    if(arr[first]<arr[second]){
+        for(int i=first; i<N-1; i++){
+            if(arr[i]>arr[i+1]) check_1 = false;
         }
     }
+    else if(arr[first]>arr[second]){
+        for(int i=first; i<N-1; i++){
+            if(arr[i]<arr[i+1]) check_2 = false;
+        }
+    }
+    if (check_1 && check_2) cout << "sorted" << endl;
+    else cout << "not sorted" << endl;
 
-    if(checker == true) cout << "sorted";
-    else cout << "Not sorted";
+    // for(int i=0; i<N-1; i++){
+    //     if(arr[i]<arr[i+1]){
+    //         checker = true;
+    //         // cout << "sorted" << " ";
+    //     }
+    //     else{
+    //         checker = false; 
+    //         // cout << "not sorted" <<  " ";
+    //         break;
+    //     }
+    // }
+
+    // if(checker == true) cout << "sorted";
+    // else cout << "Not sorted";
 }
