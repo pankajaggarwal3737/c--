c3eac5ad835ce82f91b6e81471876d10f85951c0
pankajaggarwal3737 <pankaj3737.be20@chitkara.edu.in>
2022-08-30 01:34:44 +0530

    Average of all the elements of an array

---
+++ averageOfAnArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n; cin>>n;
+    int arr[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+    
+    for(int i=0;i<n;i++){
+        sum = sum+arr[i];
+        
+
+    }
+    cout<<sum<<endl;
+    int avg=sum/n;
+    cout<<avg;
+
+}
